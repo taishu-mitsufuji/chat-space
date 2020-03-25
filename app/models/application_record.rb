439b2626437fb69b4a6f1d8a7637e284d8aac9df
taishu-mitsufuji <taishu.mitsufuji@gmail.com>
2020-03-25 16:27:21 +0900

    Update application_record.rb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
+
+class users < ActiveRecord::Base
+  has_many :users, through: :user_group
+  has_many :user_group
+end
+
+class group < ActiveRecord::Base
+  has_many :group, through: :user_group
+  has_many :user_group
+end
